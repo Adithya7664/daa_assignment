76b5f8ab1581835bd3a705ea09141c59de6a077e
M Adithya Krishna <adithyameduri2005@gmail.com>
2026-03-22 15:26:52 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-g++ -o3 -march=native daa.cpp -o bc
-
-echo "Output of Wiki-Vote dataset"
-./bc Wiki-Vote.txt < WikiVoteoutput.txt
-
-echo "Output of Email-Enron dataset"
-./bc Email-Enron.txt < EmailEnronoutput.txt
